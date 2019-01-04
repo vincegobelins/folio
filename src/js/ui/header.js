ed178c8cfa9bc2eea72576f3235214658a381c7e
Vincent <vaguettaz@gmail.com>
2019-01-04 19:29:51 +0100

    Hide menu when user scroll

--- src/js/ui/header.js
+++ src/js/ui/header.js
@@ -1,10 +1,17 @@
+import Utils from './../utils/utils';
+
 class Header {
 
     constructor(el) {
         this.el = el;
         this.menuButton = el.querySelector('.menu-button');
+        this.prevPos = 0;
+        this.isDisplayed = true;
 
         this.bindUIActions();
+
+        !Utils.isMobile() && window.addEventListener('scroll', (e) => this.handleScroll(e));
+
     }
 
     bindUIActions() {
@@ -23,6 +30,28 @@ class Header {
 
         document.dispatchEvent(event);
 
+    }
+
+    handleScroll() {
+        let pos = window.scrollY;
+        let direction = true;
+        if( this.prevPos > pos ) {
+            direction = false;
+        }
+
+        // Show menu if scroll top
+        if(!this.isDisplayed && !direction) {
+            TweenMax.staggerTo('.logo, .header-menu__link', 0.75, {opacity:1, y:0, ease: Expo.easeInOut}, 0.05);
+            this.isDisplayed = true;
+        }
+
+        // Hide menu if scroll down
+        else if ( pos > 20 && this.isDisplayed && direction ) {
+            TweenMax.staggerTo('.logo, .header-menu__link', 0.75, {opacity:0, y:-100, ease: Expo.easeInOut}, 0.05);
+            this.isDisplayed = false;
+        }
+
+        this.prevPos = pos;
     }
 }
 
